230dcbd6eaeea0bfc424518374121b73f1c10099
jxu <7989982+jxu@users.noreply.github.com>
2021-07-01 00:45:06 -0400

    Add class method documentation

--- vector.js
+++ vector.js
@@ -1,40 +1,48 @@
 // Simple Vector class, extending built-in Arrays
 class Vector extends Array {
-    // unpack if Array is passed in, otherwise just use Array constructor
+    // Unpack if Array is passed in, otherwise just use Array constructor
     constructor(...args) {
         (args.length === 1 && Array.isArray(args[0]))
             ? super(...args[0])
             : super(...args);
     }
 
+    // Add another Vector elementwise.
     add(other) {
         return this.map((e, i) => e + other[i]);
     }
 
+    // Subtract another Vector elementwise.
     sub(other) {
         return this.map((e, i) => e - other[i]);
     }
 
+    // Multiply another Vector elementwise (Hadamard product).
     mul(other) {
         return this.map((e, i) => e * other[i]);
     }
 
+    // Sum elements to produce a single result.
     sum() {
         return this.reduce((a, b) => a+b);
     }
 
+    // Scale all elements by s.
     scale(s) {
         return this.map(e => s*e);
     }
 
+    // Dot product with another Vector.
     dot(other) {
         return this.mul(other).sum();
     }
 
+    // Compute Euclidean norm.
     norm() {
         return Math.sqrt(this.dot(this));
     }
 
+    // Normalize Vector into a unit vector (norm 1).
     unit() {
         return this.scale(1 / this.norm());
     }
