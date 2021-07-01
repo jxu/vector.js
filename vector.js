7751f3c73487eea4f63bc01186ef9604f69cc34a
jxu <7989982+jxu@users.noreply.github.com>
2021-07-01 04:45:32 -0400

    Add JSDoc documentation

--- vector.js
+++ vector.js
@@ -1,12 +1,20 @@
-// Simple Vector class, extending built-in Arrays
+/**
+ * Simple Vector class, extending built-in Arrays
+ * @extends Array
+ */
 class Vector extends Array {
-    // Create Vector by unpacking Array passed in
-    // Note: passing in length 1 Array creates length 1 Vector
+    /**
+     * Create Vector.
+     * @param {Array} array - Array to be unpacked and used to create Vector.
+     *
+     * This behavior is the same for length 1 Array.
+     */
     constructor(array) {
         console.assert(Array.isArray(array),
             `Vector constructor expected Array, got ${array}`);
 
-        // Handle special case of length 1 array
+        // Handle special case of length 1 array, since Array with one numeric
+        // argument creates an Array with that many empty slots
         if (array.length === 1) {
             super();
             this.push(array[0]);
@@ -15,42 +23,71 @@ class Vector extends Array {
             super(...array);
     }
 
-    // Add another Vector elementwise.
+    /**
+     * Add elementwise.
+     * @param {Vector} other - Other summand
+     * @returns {Vector} Vector sum
+     */
     add(other) {
         return this.map((e, i) => e + other[i]);
     }
 
-    // Subtract another Vector elementwise.
+    /**
+     * Subtract elementwise.
+     * @param {Vector} other - Subtrahend
+     * @returns {Vector} Vector difference
+     */
     sub(other) {
         return this.map((e, i) => e - other[i]);
     }
 
-    // Multiply another Vector elementwise (Hadamard product).
+    /**
+     * Multiply elementwise (Hadamard product).
+     * @param {Vector} other - Other multiplicand
+     * @returns {Vector} Vector elementwise product
+     */
     mul(other) {
         return this.map((e, i) => e * other[i]);
     }
 
-    // Sum elements to produce a single result.
+    /**
+     * Sum elements.
+     * @returns {Number} Sum of elements
+     */
     sum() {
         return this.reduce((a, b) => a+b);
     }
 
-    // Scale all elements by s.
+    /**
+     * Scale elements.
+     * @param {Number} s - Factor to scale elements by
+     * @returns {Vector} Scaled vector.
+     */
     scale(s) {
         return this.map(e => s*e);
     }
 
-    // Dot product with another Vector.
+    /**
+     * Dot product.
+     * @param {Vector} other - Other vector to dot product with
+     * @returns {Number} Dot product
+     */
     dot(other) {
         return this.mul(other).sum();
     }
 
-    // Compute Euclidean norm.
+    /**
+     * Compute Euclidean (L2) norm.
+     * @returns {number} Euclidean norm
+     */
     norm() {
         return Math.sqrt(this.dot(this));
     }
 
-    // Normalize Vector into a unit vector (norm 1).
+    /**
+     * Normalize (scale) to a unit vector.
+     * @returns {Vector} Unit vector
+     */
     unit() {
         return this.scale(1 / this.norm());
     }
